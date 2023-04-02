80e99b875b469b7df27bd0d9ef272c85623fc634
Justim Odendaal <justim.odendaal@gmail.com>
2023-04-02 15:44:37 +0100

    WIP

--- src/Azure.CognitiveService.Client.BlazorApp/Program.cs
+++ src/Azure.CognitiveService.Client.BlazorApp/Program.cs
@@ -2,9 +2,12 @@ using Azure.CognitiveService.Client.BlazorApp.Models;
 using Azure.CognitiveServices.Client;
 using Azure.CognitiveServices.Client.OpenAI.Models;
 using BlazorStrap;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Polly;
 using Polly.Extensions.Http;
+using System.Security.Claims;
 
 namespace Azure.CognitiveService.Client.BlazorApp
 {
@@ -48,6 +51,44 @@ namespace Azure.CognitiveService.Client.BlazorApp
                 o.DeploymentName = e.Value.Chat.DeploymentName;
             });
 
+            //Authentication
+            builder.Services
+                .AddAuthentication()
+                .AddOpenIdConnect("oidc", "Identity Server", options =>
+            {
+                options.Prompt = builder.Configuration["IdentityServer:Prompt"].ToString();
+                options.Authority = builder.Configuration["IdentityServer:Url"].ToString();
+                options.ClientId = builder.Configuration["IdentityServer:ClientId"].ToString();
+                options.ClientSecret = builder.Configuration["IdentityServer:Secret"].ToString();
+                options.ResponseType = "code";
+                options.ResponseMode = "query";
+                options.SaveTokens = true;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("email");
+                options.Scope.Add("profile");
+
+                options.Scope.Add("offline_access");
+                options.ClaimActions.MapJsonKey(claimType: ClaimTypes.Email, jsonKey: "email");
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
+            }
+        );
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.Unspecified;
+            });
+
+            builder.Services.AddAntiforgery(opts => {
+                opts.Cookie.SameSite = SameSiteMode.Unspecified;
+            });
+
+
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
@@ -69,6 +110,9 @@ namespace Azure.CognitiveService.Client.BlazorApp
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
